169cd1e42da2c4952ac1d8404ba4adb229413e8d
Thomas Vinicius de Moraes <tho.vini7@gmail.com>
2026-08-20 18:49:05 -0300

    Secrets

--- server.go
+++ server.go
@@ -1,12 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
+
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s, Age: %s\n", name, age)
+
 	w.Write([]byte("<h1>Hello Full Cycle!</h1>"))
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My family: %s\n", string(data))
 }
